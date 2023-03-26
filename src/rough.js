240b50bea8d9ff8d11a850f225113cc6791217fb
Javed Alam <jalam@redhat.com>
2023-03-26 15:40:16 +0530

    Confitional return learning

--- src/rough.js
+++ src/rough.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 // don't change the Component name "App"
 export default function App() {
-    const [value, setValue] = React.useState('Invalid');
-    const validationHandler = (event) => {
-        let char_value = event.target.value;
-        if (char_value.trim().length < 3) {
-            setValue("Invalid")
-        } else {
-            setValue("Valid")
-        }
-    };
-    
-    return (
-        <form>
-            <label>Your message</label>
-            <input type="text" onChange={validationHandler}/>
-            <p>{value} message</p>
-        </form>
-    );
+  const [value, setValue] = React.useState("Invalid");
+  const validationHandler = (event) => {
+    let char_value = event.target.value;
+    if (char_value.trim().length < 3) {
+      setValue("Invalid");
+    } else {
+      setValue("Valid");
+    }
+  };
+
+  return (
+    <form>
+      <label>Your message</label>
+      <input type="text" onChange={validationHandler} />
+      <p>{value} message</p>
+    </form>
+  );
 }
 
 /*
@@ -54,3 +54,43 @@ export default function App() {
 }
 
 */
+
+/**
+ * On Delete Warning Show Module
+ * 
+ * 
+ import React from 'react';
+
+// don't change the Component name "App"
+export default function App() {
+    
+    const [isDeleting, setIsDeleting] = React.useState(false);
+    
+    function deleteHandler() {
+        setIsDeleting(true);
+    }
+    
+    function proceedHandler() {
+        setIsDeleting(false);
+    }
+    let warning;
+    if (isDeleting) {
+        warning =       (
+                            <div id="alert">
+                              <h2>Are you sure?</h2>
+                              <p>These changes can't be reverted!</p>
+                              <button onClick={proceedHandler}>Proceed</button>
+                            </div>
+                        )
+                    }
+
+    
+    return (
+      <div>
+            {warning}
+        
+        <button onClick={deleteHandler}>Delete</button>
+      </div>    
+    );
+}
+ */
