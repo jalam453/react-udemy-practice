b0fe96d920fb2c4ec8eb9e2823db34ffccddb4fa
Javed Alam <jalam@redhat.com>
2023-05-08 21:29:09 +0530

    Conditional form Handler, L-68+d

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  //   console.log(title);
+  // };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <h2 className="expense-item__description">{title}</h2>
-      <div className="expense-item__price">{props.amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+          {/* <button onClick={clickHandler}>Change Title</button> */}
+        </div>
+      </Card>
+    </li>
   );
 }
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpanse = (props) => {
-    const saveExpenseHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id : Math.random().toString()
-        };
-        // console.log(expenseData);
-        props.onAddExpense(expenseData);
-
+  const [isEditing, setEditing] = useState(false);
+  const saveExpenseHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
     };
-    
-    return ( 
-    <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
+    // console.log(expenseData);
+    props.onAddExpense(expenseData);
+    setEditing(false);
+  };
 
+  const startEditingHandler = () => {
+    setEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setEditing(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
-    );
+  );
 };
 
 export default NewExpanse;
